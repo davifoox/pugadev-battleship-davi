450d6135ef52d33ee1386221dfc36f596c43bb96
Davi Fox <davifoox@gmail.com>
2020-01-31 12:58:28 -0300

    Add Game Over/ Won message at ending screen

---
+++ pugadev-puga-battleship-test-3228d920d2d0/Assets/Scripts/UI/CanvasEnding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasEnding : MonoBehaviour
+{
+
+    public Text endingMessage;
+
+    private void Start()
+    {
+        if (GameManager.Instance.playerWon)
+        {
+            endingMessage.text = "WON";
+        }
+        else
+        {
+            endingMessage.text = "GAME OVER";
+        }
+    }
+}
